cd0520eda04446f2594601b8e96425f029d4d8f6
CS4850_Red_ASMCyberProject <llado95@gmail.com>
2026-04-05 17:09:19 -0400

    Update Normalize_Slack_Thread_TS.py

--- Shuffle_Workflow_Main/Python_Scripts/Normalize_Slack_Thread_TS.py
+++ Shuffle_Workflow_Main/Python_Scripts/Normalize_Slack_Thread_TS.py
@@ -1,9 +1,10 @@
 import json, base64
 
-#The data passed into the script is raw exec data, we
-#convert it to json then to exec_b64 so that there aren't any
-#formatting or type errors while passing the data in Shuffle.
-#Also we use a r string so that the formatting is more legible.
+#Normalize the output of Slack Cases and Slack Alerts needed to set the thread timestamps of both channels in Slack 
+#Each alert will only be posted to either Slack Alerts or Slack Cases, each of which has a single node in Slack for posting.
+#Normalize Slack Thread TS Script simply gets the payload from each node and puts it into a varaible slack_alert or slack_case.
+#We then normalize the output of both results by putting it into a variable called alert using a conditional because only 1 variable will have output.
+#The other will be empty. 
 slack_alert = json.loads(r'''{{$sp-108-redasm_cases.body | default({}) | tojson}}''')
 
 slack_case = json.loads(r'''{{$sp-108-redasm_alerts.body | default({}) | tojson}}''')
