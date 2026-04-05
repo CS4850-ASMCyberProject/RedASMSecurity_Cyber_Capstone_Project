2f7c5d224d3774dbd9caa049122cfd297fde1d17
CS4850_Red_ASMCyberProject <llado95@gmail.com>
2026-04-05 17:18:13 -0400

    Update Normalize_User.py

--- Shuffle_Workflow_Main/Python_Scripts/Normalize_User.py
+++ Shuffle_Workflow_Main/Python_Scripts/Normalize_User.py
@@ -1,15 +1,18 @@
 import json, base64
 
-#The data passed into the script is raw $exec data, we
-#convert it to json then to exec_b64 so that there aren't any
-#formatting or type errors while passing the data in Shuffle.
-#Also we use a r string so that the formatting is more legible.
+# Get the original wazuh alert
 exec_b64 = r'''{{ $exec | tojson | base64_encode }}'''
 
+# Convert to a Json string
 data = json.loads(base64.b64decode(exec_b64).decode("utf-8"))
 
+# Get the source user for certain alerts which is located in a different place than other alerts 
+# Normalize the call. Most users are stored at srcuser, but some are at dstuser
+# If it's an alert stored at dstuser, then normalize it as a variable called source_user
 source_user = data.get("all_fields", {}).get("data", {}).get("srcuser", "") or data.get("all_fields", {}).get("data", {}).get("dstuser", "")
 
+# If the source user is empty, meaning it's not an alert that stores the user at dstuser, then pass
+# Else, add the source_user to a key source_user
 if not source_user:
   pass
 else:
