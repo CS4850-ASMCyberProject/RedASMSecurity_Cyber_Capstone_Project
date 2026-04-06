e53e5e0fab1802b1377d6ee938f9fdeefdd2811a
CS4850_Red_ASMCyberProject <llado95@gmail.com>
2026-04-05 21:29:25 -0400

    Update Update_Corrkey.py

--- Shuffle_Workflow_Main/Python_Scripts/Update_Corrkey.py
+++ Shuffle_Workflow_Main/Python_Scripts/Update_Corrkey.py
@@ -1,19 +1,21 @@
 import json, base64
 
-#The data passed into the script is raw $exec data, we
-#convert it to json then to exec_b64 so that there aren't any
-#formatting or type errors while passing the data in Shuffle.
-#Also we use a r string so that the formatting is more legible.
+#Get the custom python script alert in Shuffle. Use this encoding convert to base 64 for safe handling in Shuffle
 decoded = r'''{{ $python_slack_script.message | tojson | base64_encode }}'''
-
 alert = base64.b64decode(decoded).decode("utf-8")
 
+#Convert to Json String 
 alert = json.loads(alert)
 
+#Get the Corrkey
 corrkey = alert.get("corrkey", "")
 
+#Add the thread timestamp to the corrkey for thehive 
+#This is similar to Slack Threading, where alerts are threaded together if it's a related alert set off within the last 5 minutes
+#Else the Corrkey adds a new parent timestamp to create a new alert in thehive
 update_corrkey = corrkey + ":" + "$set_new_thread_ts.value.Set_Thread_TS"
 
+#Update Corrkey in the alert payload
 alert["corrkey"] = update_corrkey
 
 #Print json.dumps with the alert 
